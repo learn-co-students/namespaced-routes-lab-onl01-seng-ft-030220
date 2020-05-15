e1efd05a67d7d7f1753b43b2a331585b4a953199
Jacqueline Freeland <secretartist272@gmail.com>
2020-05-15 15:45:05 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,14 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+   
+    @pref = Preference.first
+
+    if @pref.allow_create_songs == true
+      @song = Song.new
+    else
+      redirect_to songs_path
+    end
   end
 
   def create
